1b16dda96434a6595fc154dc1958fee1f87d232a
vank <roger.van.koughnett@gmail.com>
2012-03-08 14:43:32 -0500

    tabbs now show current

--- js/ajax-intro.js
+++ js/ajax-intro.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
     /*
 	AJAX is the process where we call information direct from the server without passing through a PHP so to load other information eg an html php+mySQL result etc
 	*/
+	$(".tab-buttons a").on("click", function(ev){
+				
+		$('.tab-buttons .active').removeClass('active');
+		$(this).addClass('active');
+
+	});
 	
 	$(".ajax-in1").on("click", function (ev) {
 	$(".fill-me").hide();
